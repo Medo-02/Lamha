dfcaacd053b536f0b04b68abacbb9bcd1e0c343f
Mohammed alzowaid <Moha-a-36@hotmail.com>
2024-11-12 22:21:39 +0300

    Add draw polygon feature

--- src/app/components/mapbox/mapbox.component.ts
+++ src/app/components/mapbox/mapbox.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import mapboxgl from 'mapbox-gl';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 
 @Component({
@@ -15,39 +15,51 @@ export class MapboxComponent implements OnInit {
   map!: mapboxgl.Map;
   draw!: MapboxDraw;
 
+  constructor() {
+    // Move MapboxDraw initialization to ngOnInit
+  }
+
   ngOnInit() {
+    // Set access token
     mapboxgl.accessToken =
       'REMOVED';
+
+    // Set RTL text plugin
     mapboxgl.setRTLTextPlugin(
       'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.3.0/mapbox-gl-rtl-text.js',
       null,
-      true // Lazy load the plugin
+      true
     );
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+
+    // Initialize map
+    this.map = new mapboxgl.Map({
+      container: 'map',
       style: 'mapbox://styles/mapbox/traffic-night-v2',
-      center: [46.7219, 24.6877], // Riyadh coordinates
-
-      zoom: 9, // starting zoom
+      center: [46.7219, 24.6877],
+      zoom: 9,
     });
 
-    // Add navigation controls to the map
-    this.map.addControl(new mapboxgl.NavigationControl());
+    // Initialize draw after map is loaded
+    this.map.on('load', () => {
+      // Initialize MapboxDraw
+      this.draw = new MapboxDraw({
+        displayControlsDefault: false,
+        controls: {
+          polygon: true,
+          trash: true,
+        },
+        defaultMode: 'draw_polygon',
+      });
 
+      // Add draw control to map
+      this.map.addControl(this.draw);
 
-    this.draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-        polygon: true, // Enable the polygon drawing tool
-        trash: true, // Enable the delete tool
-      },
+      // Add event listeners
+      this.map.on('draw.create', this.updatePolygonCoordinates.bind(this));
+      this.map.on('draw.update', this.updatePolygonCoordinates.bind(this));
     });
-    this.map.addControl(this.draw);
-
-    // Event listener for when a polygon is created or updated
-    this.map.on('draw.create', this.updatePolygonCoordinates.bind(this));
-    this.map.on('draw.update', this.updatePolygonCoordinates.bind(this));
   }
+
   updatePolygonCoordinates(event: any): void {
     // Get all drawn features
     const data = this.draw.getAll();
