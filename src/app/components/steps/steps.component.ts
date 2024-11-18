d9ee3d89098ff2a23b4b37256386b41ae4f94d99
Mohammed alzowaid <moha-a-36@hotmail.com>
2024-11-18 14:57:01 +0300

    Animate the steps in the landing page

--- src/app/components/steps/steps.component.ts
+++ src/app/components/steps/steps.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 @Component({
   selector: 'app-steps',
   standalone: true,
@@ -7,6 +9,54 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './steps.component.html',
   styleUrl: './steps.component.scss'
 })
-export class StepsComponent {
+export class StepsComponent implements OnInit {
+  constructor(private translateService: TranslateService) { }
+  
+  ngOnInit(): void {
+    const isArabic = this.translateService.currentLang === 'ar';
+    gsap.registerPlugin(ScrollTrigger);
 
+    //step 1 animation
+    gsap.set('#step1', { opacity: 0, x: isArabic ? -250 : 250 });
+    gsap.to('#step1', {
+      scrollTrigger: {
+        trigger: '#step1',
+        start: 'top center',
+        toggleActions: 'play none none none',
+      },
+      opacity: 1,
+      x: 0,
+      duration: 1,
+      ease: 'back.out(1.7)',
+    });
+
+    //step 2 animation
+    gsap.set('#step2', { opacity: 0, x: isArabic ? 250 : -250 });
+    gsap.to('#step2', {
+      scrollTrigger: {
+        trigger: '#step2',
+        start: 'top center',
+        toggleActions: 'play none none none',
+      },
+      opacity: 1,
+      x: 0,
+      duration: 1,
+      ease: 'back.out(1.7)',
+    });
+
+    //step 3 animation
+    gsap.set('#step3', { opacity: 0, x: isArabic ? -250 : 250 });
+    gsap.to('#step3', {
+      scrollTrigger: {
+        trigger: '#step3',
+        start: 'top center',
+        toggleActions: 'play none none none',
+      },
+      opacity: 1,
+      x: 0,
+      duration: 1,
+      ease: 'back.out(1.7)',
+    });
+  }
+  
 }
